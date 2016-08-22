35f70c4953e90c2d48708aa906f31b2a89a11405
Antonio Carlos Pina <antoniocarlospina@gmail.com>
2016-08-22 20:46:15 -0300

    cachematrix.R exercise from Coursera

---
+++ ProgrammingAssignment2/cachematrix.R
@@ -0,0 +1,33 @@
+
+## Creates the Special "Matrix" and caches it
+
+makeCacheMatrix <- function(x = matrix()) {
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setsolve <- function(solve) m <<- solve
+  getsolve <- function() m
+  list(set = set, get = get,
+       setsolve = setsolve,
+       getsolve = getsolve)
+}
+
+
+## Solves the Matrix but befores check the cache. If exists,
+##then recall it from the cache
+
+cacheSolve <- function(x, ...) {
+        ## Return a matrix that is the inverse of 'x'
+  m <- x$getsolve()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setsolve(m)
+  m
+}
